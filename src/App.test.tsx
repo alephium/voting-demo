3984b6950b4b60a10c4a372de3ce084178301135
Boubacar Camara <bakarsadio@gmail.com>
2021-12-09 11:22:32 +0100

    Add basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
+import Client from './util/client'
+
+jest.mock('./util/client')
+window.alert = jest.fn()
 
 test('renders the home page', () => {
   render(<App />)
@@ -9,4 +14,36 @@ test('renders the home page', () => {
     const element = screen.getByText(text)
     expect(element).toBeInTheDocument()
   })
+})
+
+test('navigates to Vote', () => {
+  render(<App />)
+  userEvent.click(screen.getByText('Vote'))
+  expect(screen.getByText('Load Contract')).toBeInTheDocument()
+})
+
+test('navigates to Administrate', () => {
+  render(<App />)
+  userEvent.click(screen.getByText('Administrate'))
+  expect(screen.getByText('Allocate Tokens')).toBeInTheDocument()
+})
+
+test('navigates to settings', () => {
+  render(<App />)
+  userEvent.click(screen.getByText('Settings'))
+  expect(screen.getByText('Wallet Settings')).toBeInTheDocument()
+})
+
+test('unlock wallet', () => {
+  window.alert.mockClear()
+  Client.mockClear()
+  const walletUnlock = jest.fn().mockResolvedValue(() => Promise.resolve('dummy result'))
+  Client.mockImplementation(() => {
+    return {
+      walletUnlock: walletUnlock
+    }
+  })
+  render(<App />)
+  userEvent.click(screen.getByText('Unlock Wallet'))
+  expect(walletUnlock).toHaveBeenCalledTimes(1)
 })
