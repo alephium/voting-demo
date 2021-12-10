2810dd43bfbd0dcfb74cb50234a2fdf434c86088
Thomas Droxler <thomas.droxler@gmail.com>
2021-12-10 09:14:11 +0100

    Add better error filtering in `catchAndAlert`

--- src/util/util.tsx
+++ src/util/util.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
 export function catchAndAlert(action: Promise<any>) {
   action.catch((e) => {
-    if (e.error.detail != undefined) {
+    if (e.error != undefined && e.error.detail != undefined) {
       alert(e.error.detail)
     } else {
       alert(e)
