fd854045a956ea7aef84acd7e128b93855ae2c53
Boubacar Camara <bakarsadio@gmail.com>
2021-12-17 10:27:44 +0100

    Improve catchAndAlert

--- src/util/util.tsx
+++ src/util/util.tsx
@@ -4,7 +4,7 @@ import { TypedStatus } from './types'
 // eslint-disable-next-line
 export function catchAndAlert(action: Promise<any>) {
   action.catch((e) => {
-    if (e.error != undefined && e.error.detail != undefined) {
+    if (e != undefined && e.error != undefined && e.error.detail != undefined) {
       alert(e.error.detail)
     } else {
       alert(e)
